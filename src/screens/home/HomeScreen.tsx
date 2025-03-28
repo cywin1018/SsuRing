aa12e2fbc6864c67c451758d317ce9b79840f483
Choi Yong Won <cywin0821@gmail.com>
2025-03-28 15:23:56 +0900

    chore : 로그인 코드 추가, 홈 화면 추가

---
+++ src/screens/home/HomeScreen.tsx
@@ -0,0 +1,4 @@
+const HomeScreen = () => {
+  return <div className={'bg-amber-500'}>dsdsds</div>;
+};
+export default HomeScreen;
--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import useAuthStore from '../../stores/LoginStore.ts';
-import {Button, Switch, Text, TextInput, View} from 'react-native';
+import {Alert, Button, Switch, Text, TextInput, View} from 'react-native';
 
 const LoginScreen = () => {
   const {studentId, password, autoLogin, setCredentials, setAutoLogin} =
     useAuthStore();
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://10.0.2.2:8000/login', {
+        studentId: studentId,
+        password: password,
+      });
+      console.log(response);
+      console.log('Login successful:', response.data);
+
+      Alert.alert(
+        '로그인 성공',
+        '크롤링 데이터: ' + JSON.stringify(response.data.programs),
+      );
+    } catch (error) {
+      console.error(error);
+      Alert.alert('로그인 실패', '로그인에 실패했습니다.');
+    }
+  };
 
   return (
     <View className="flex-1 items-center justify-center">
@@ -34,13 +53,7 @@ const LoginScreen = () => {
           <Text className="ml-2 text-base">자동 로그인</Text>
         </View>
         <View className="mt-4">
-          <Button
-            title="로그인"
-            onPress={() => {
-              /* 로그인 처리 */
-              console.log(studentId, password);
-            }}
-          />
+          <Button title="로그인" onPress={handleLogin} />
         </View>
       </View>
     </View>
