388bf60569bd475131f3d306bf9bfa949ee506c9
Choi Yong Won <cywin0821@gmail.com>
2025-03-29 10:56:05 +0900

    feat : 로그인 관련 store 코드 추가

--- src/stores/LoginStore.ts
+++ src/stores/LoginStore.ts
@@ -1,11 +1,22 @@
 import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {AuthProps} from '../types/login/login.ts';
 
-const useAuthStore = create<AuthProps>(set => ({
-  studentId: '',
-  password: '',
-  autoLogin: false,
-  setCredentials: (studentId, password) => set({studentId, password}),
-  setAutoLogin: value => set({autoLogin: value}),
-}));
+const useAuthStore = create<AuthProps>()(
+  persist(
+    set => ({
+      studentId: '',
+      password: '',
+      autoLogin: false,
+      setCredentials: (studentId, password) => set({studentId, password}),
+      setAutoLogin: value => set({autoLogin: value}),
+      reset: () => set({studentId: '', password: '', autoLogin: false}),
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 export default useAuthStore;
